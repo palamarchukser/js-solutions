dd939d9326ac49608e9a4b93e7c5b235fea471a4
palamarchukser <serhii.palamarchuk@chess.com>
2022-08-06 17:31:06 +0300

    Check whether it is possible to apply the swap operation at most once

--- Rectangular container of dimensions/solution.js
+++ Rectangular container of dimensions/solution.js
@@ -16,7 +16,9 @@ const solution = (blocks, height) => {
         s = s + blocks[i];
     }
 
-    let wMin = Math.ceil(s / h);
+    let wMin1 = Math.ceil(s / h);
+    let wMin2 = Math.max.apply(null, b);
+    let wMin = wMin1 > wMin2 ? wMin1 : wMin2;
 
     while (str > h){
         let n = 0;
@@ -28,7 +30,12 @@ const solution = (blocks, height) => {
             while (sum < wMin && n < b.length){
                 sum += b[n];
                 if (sum > wMin) {
-                    sum -= b[n];
+                    if (n===0) {
+                        wMin = b[0]
+                        n = n+1;
+                    } else {
+                        sum -= b[n];
+                    }
                     break;
                 } else {
                     n = n+1;
@@ -48,3 +55,43 @@ const solution = (blocks, height) => {
 
 console.log(solution([1, 3, 1, 3, 3], 2)) //6
 console.log(solution([2, 3, 1, 1, 1], 2)) //5
+console.log(solution([4, 1, 1], 2)) //4
+console.log(solution([1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 100000)) //1
+
+
+/*
+Input:
+    blocks: [4, 1, 1]
+height: 2
+Expected Output:
+    4
+
+
+Input:
+blocks: [3]
+height: 2
+Expected Output:
+3
+
+
+
+
+
+ Input:
+blocks: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+height: 1
+Expected Output:
+10
+
+
+
+
+Input:
+blocks: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+height: 1000000000
+Output:
+undefined
+Expected Output:
+1
+
+*/
